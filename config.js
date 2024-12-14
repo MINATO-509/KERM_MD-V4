ceab8ffd6fccd3b85afc75be28426943330de819
MINATO-509 <minatomr00@gmail.com>
2024-12-13 20:39:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/osriwc.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR IM ONLINE I'M KERM_MD-V4 WHATSAPP BOT\n\n> 𝖡𝖸 𝖪𝖦𝖳𝖤𝖢𝖧",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || "-",
+PREFIX: process.env.PREFIX || "☆",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "➺『🌹𝗠𝗥 𝗠𝗜𝗡𝗔𝗧𝗢 𝗠𝗗 𝗩4🌹』࿐",
+BOT_NAME: process.env.BOT_NAME || "➺『🌹𝗠𝗥 𝗠𝗜𝗡𝗔𝗧𝗢 𝗩4🌹』࿐",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
